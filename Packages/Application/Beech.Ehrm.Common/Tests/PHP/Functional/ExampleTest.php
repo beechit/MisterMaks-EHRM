bde524aabea83804551ac6a72824488851845ebe
Rens Admiraal <rens@beech.it>
2012-04-24 19:30:38 +0200

    [TASK] Alter build script
    - Comment TYPO3.* code sniffs
    - Correct some paths
    
    Change-Id: Icd49fc72c17d89c07635660b7f59d016a5d9a249

---
+++ Packages/Application/Beech.Ehrm.Common/Tests/PHP/Functional/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Beech\Ehrm\Common\Functional;
+
+/**
+ * Test suite for Signal Slot
+ *
+ */
+class Example extends \TYPO3\FLOW3\Tests\FunctionalTestCase {
+
+	/**
+	 * @test
+	 */
+	public function thisIsAnExampleTest() {
+		$this->assertTrue(TRUE, 'from abstract class');
+	}
+
+}
+?>
