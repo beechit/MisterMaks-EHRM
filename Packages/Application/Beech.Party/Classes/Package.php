cbe4cc1fcd385e64df9956d1a33ef5dfa64a7327
Rens Admiraal <rens@beech.it>
2012-06-26 14:02:13 +0200

    [TASK] Add Beech.Party submodule
    Change-Id: Ia9ac077a9687be417e0d5e4b35da6e94b908cde9

---
+++ Packages/Application/Beech.Party/Classes/Package.php
@@ -0,0 +1,22 @@
+<?php
+namespace Beech\Party;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Developer: Rens Admiraal <rens@beech.it>
+ * Date: 26-06-12 13:53
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\FLOW3\Package\Package as BasePackage;
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Package base class of the Beech.Party package.
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class Package extends BasePackage {
+
+}
+?>
