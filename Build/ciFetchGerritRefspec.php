2f666e89be41b073df6f24e1e1d15262a765ed53
Rens Admiraal <rens@beech.it>
2012-07-02 21:52:43 +0200

    [TASK] Fix workspace path error in CI patch script
    Change-Id: I7c211b63c86ab0850720ba40324639964c68fc92

--- Build/ciFetchGerritRefspec.php
+++ Build/ciFetchGerritRefspec.php
@@ -3,24 +3,58 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_STRICT);
 
-$project		= getenv('GERRIT_PROJECT');
-$path			= NULL;
-$gerritHost		= getenv('GERRIT_HOST');
-$refSpec		= getenv('GERRIT_REFSPEC');
+if (!getenv('GERRIT_PROJECT')) {
+	echo 'GERRIT_PROJECT env var not set';
+	exit(1);
+}
 
-$workspaceRemoteUrl = shell_exec(sprintf("cd %s && git config remote.origin.url | grep '%s.git'", getenv('WORKSPACE'), $project));
+if (!getenv('GERRIT_HOST')) {
+	echo 'GERRIT_HOST env var not set';
+	exit(1);
+}
+
+if (!getenv('JENKINS_HOST')) {
+	echo 'JENKINS_HOST env var not set';
+	exit(1);
+}
+
+if (!getenv('GERRIT_REFSPEC')) {
+	echo 'GERRIT_REFSPEC env var not set';
+	exit(1);
+}
+
+if (!getenv('WORKSPACE')) {
+	echo 'WORKSPACE env var not set';
+	exit(1);
+}
+
+$project = getenv('GERRIT_PROJECT');
+$path = NULL;
+$gerritHost = getenv('GERRIT_HOST');
+$jenkinsHost = getenv('JENKINS_HOST');
+$refSpec = getenv('GERRIT_REFSPEC');
+$workspace = dirname(getenv('WORKSPACE')) . DIRECTORY_SEPARATOR . 'workspace' . DIRECTORY_SEPARATOR;
+
+$workspaceRemoteUrl = shell_exec(sprintf("cd %s && git config remote.origin.url | grep '%s.git'", $workspace, $project));
 if ($workspaceRemoteUrl !== NULL) {
-	$path = getenv('WORKSPACE');
+	echo 'GERRIT_PROJECT found in base distribution';
+	$path = $workspace;
 } else {
 	/**
 	 * Traverse over all submodules, and check if remote.origin.url matches the project name, if so, set the path
 	 */
-	$submodules = shell_exec(sprintf("cd %s && git submodule foreach --recursive 'git config remote.origin.url | grep \"%s.git\" || :'", getenv('WORKSPACE'), $project));
-	$submodules = explode(chr(10), $submodules);
-
+	$submodules = shell_exec(sprintf("cd %s && git submodule foreach --recursive 'git config remote.origin.url | grep \"%s.git\" || :'", $workspace, $project));
+	$submodules = explode(chr(10), trim($submodules));
 	foreach ($submodules as $index => $submodule) {
 		if (!empty($submodule) && substr($submodule, 0, 9) !== 'Entering ') {
-			$path = realpath(getenv('WORKSPACE') . str_replace(array('Entering ', "'"), '', $submodules[$index - 1]));
+			$submodulePath = trim(str_replace(array('Entering', "'"), '', $submodules[$index - 1]));
+			echo sprintf('GERRIT_PROJECT found in submodule path %s', $submodulePath) . chr(10);
+			$path = realpath($workspace . $submodulePath) . DIRECTORY_SEPARATOR;
+
+			if (getenv('JOB_NAME') && getenv('BUILD_NUMBER') && getenv('GERRIT_CHANGE_SUBJECT')) {
+				shell_exec(sprintf("ssh %s set-build-description %s %s '%s'", $jenkinsHost, getenv('JOB_NAME'), getenv('BUILD_NUMBER'), getenv('GERRIT_CHANGE_SUBJECT')));
+			}
+			continue;
 		}
 	}
 }
@@ -30,11 +64,7 @@ if ($path !== NULL) {
 	 * Create the checkout command and execute it in the found path
 	 */
 	$checkoutCommand = sprintf('git fetch ssh://%s/%s %s && git checkout FETCH_HEAD', $gerritHost, $project, $refSpec);
-	echo '*********************' . chr(10);
-	echo '* Download patchset *' . chr(10);
-	echo '*********************' . chr(10);
 	echo 'Command: ' . $checkoutCommand . chr(10);
-	echo 'Path: ' . $path . chr(10);
 	echo shell_exec(sprintf('cd %s && %s', $path, $checkoutCommand)) . chr(10) . chr(10);
 } else {
 	echo 'Refspec could not be checked out in any local module' . chr(10);
