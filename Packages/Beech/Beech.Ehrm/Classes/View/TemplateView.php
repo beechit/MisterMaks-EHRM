c6ccbe94221c06009f4857db9914e6551ad014e7
karol <karol@beech.it>
2012-08-01 15:57:26 +0200

    [TASK] Add support for loading multiple Partial Templates
    It adds support for loading default partial template from Beech.Ehrm
    if it not exists in actual package.
    Additionally gives possibility to load template for absolute path.
    So it possible now to load partial template from any package.
    
    Resolves: #463, #464
    Change-Id: I55efa5b8133467054e288be3f7be18d40b67ce2c

--- Packages/Beech/Beech.Ehrm/Classes/View/TemplateView.php
+++ Packages/Beech/Beech.Ehrm/Classes/View/TemplateView.php
@@ -1,16 +1,19 @@
 <?php
 namespace Beech\Ehrm\View;
-
 /*
  * This source file is proprietary property of Beech Applications B.V.
  * Developer: Rens Admiraal <rens@beech.it>
  * Date: 23-07-12 23:55
  * All code (c) Beech Applications B.V. all rights reserved
  */
-
-/**
- */
 class TemplateView extends \TYPO3\Fluid\View\TemplateView {
+
+	/**
+	 * Directory pattern for global partials.
+	 *
+	 * @var string
+	 */
+	private $partialPathAndFilenamePattern = '@partialRoot/@subpackage/@partial.@format';
 
 	/**
 	 * Resolve the path and file name of the layout file, based on
@@ -32,8 +35,44 @@ class TemplateView extends \TYPO3\Fluid\View\TemplateView {
 			$this->layoutRootPathPattern = str_replace('@packageResourcesPath', 'resource://Beech.Ehrm', $this->layoutRootPathPattern);
 			$layoutPathAndFilename = parent::getLayoutPathAndFilename($layoutName);
 		}
-
 		return $layoutPathAndFilename;
+	}
+
+	/**
+	 * Resolve the partial path and filename based on $this->partialPathAndFilenamePattern.
+	 * Overrides a parent method to support loading partial templates from the Beech.Ehrm package.
+	 *
+	 * Usage:
+	 * 1) <f:render partial="PartialTemplateName"/>
+	 * a) Is looking for template <PackageName>/Resources/Private/Partials/PartialTemplateName(.html)
+	 * b) If template is not found it starts to look inside default package
+	 * Beech.Ehrm/Resources/Private/Partials/PartialTemplateName(.html)
+	 *
+	 * 2) Now it supports also absolute, resource paths, ex:
+	 * <f:render partial="resource://Beech.Ehrm/Private/Partials/PartialTemplateName.html"/>
+	 *
+	 * @param string $partialName The name of the partial
+	 * @return string the full path which should be used. The path definitely exists.
+	 * @throws \TYPO3\Fluid\View\Exception\InvalidTemplateResourceException
+	 */
+	protected function getPartialPathAndFilename($partialName) {
+		try {
+			return parent::getPartialPathAndFilename($partialName);
+		} catch (\TYPO3\Fluid\View\Exception\InvalidTemplateResourceException $exception) {
+				//set path to default package
+			$this->setPartialRootPath('resource://Beech.Ehrm/Private/Partials');
+			$defaultPaths = $this->expandGenericPathPattern($this->partialPathAndFilenamePattern, TRUE, TRUE);
+				//additionally, extra check
+			$extraPath = array($partialName);
+			$paths = array_merge($defaultPaths, $extraPath);
+			foreach ($paths as &$partialPathAndFilename) {
+				$partialPathAndFilename = str_replace('@partial', $partialName, $partialPathAndFilename);
+				if (file_exists($partialPathAndFilename)) {
+					return $partialPathAndFilename;
+				}
+			}
+		}
+		throw new \TYPO3\Fluid\View\Exception\InvalidTemplateResourceException('The template files for partial "' . $partialName . '" could not be loaded.', 1225709595);
 	}
 }
 
