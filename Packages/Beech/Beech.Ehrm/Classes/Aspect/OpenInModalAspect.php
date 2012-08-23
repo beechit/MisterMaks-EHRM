3a28110d216c9562727a86b4210cf2b9aaa167fa
karol <karol@beech.it>
2012-08-23 12:43:30 +0200

    [TASK] Support for open menu link in modal box
    Change-Id: I169d78bed06007a5e92699105606cfc8d1766d3f
    Resolves: #523

---
+++ Packages/Beech/Beech.Ehrm/Classes/Aspect/OpenInModalAspect.php
@@ -0,0 +1,50 @@
+<?php
+namespace Beech\Ehrm\Aspect;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Developer: Karol Kaminski <karol@beech.it>
+ * Date: 23-08-12 10:54
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\DomCrawler;
+
+/**
+ * Open link in modal box functionality
+ *
+ * @FLOW3\Aspect
+ */
+class OpenInModalAspect {
+
+	/**
+	 * Parse generated menu to add support for opening links in modal box.
+	 * If is set option 'modal: TRUE', open menu link in modal.
+	 *
+	 * By setting option modalId, its possible to open content in specified modal.
+	 *
+	 * @FLOW3\Around("method(Twitter\Bootstrap\ViewHelpers\Navigation\MenuViewHelper->render())")
+	 * @param \TYPO3\FLOW3\AOP\JoinPointInterface $joinPoint
+	 * @return string
+	 */
+	public function addModalArguments(\TYPO3\FLOW3\AOP\JoinPointInterface $joinPoint) {
+		$result = $joinPoint->getAdviceChain()->proceed($joinPoint);
+		$items = $joinPoint->getMethodArgument('items');
+		foreach ($items as $item) {
+			if (isset($item['modal']) && $item['modal'] === TRUE) {
+				$crawler = new \Symfony\Component\DomCrawler\Crawler($result);
+				$crawler = $crawler->filterXPath('//a[@href="' . $item['href'] . '"]');
+				foreach ($crawler as $domElement) {
+					$domElement->setAttribute('data-toggle', 'modal');
+					$domElement->setAttribute('data-target', '#' . (isset($item['modalId']) ? $item['modalId'] : 'entityModal'));
+					$result = $domElement->ownerDocument->saveHTML();
+				}
+			}
+		}
+		return preg_replace('/<!DOCTYPE [^>]+>|<(\/){0,1}(html|body|_root)>/', '', $result);
+	}
+}
+
+?>
