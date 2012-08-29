a7af7083ee8cea10868069f0f3f6b0ff7f4cd57c
karol <karol@beech.it>
2012-08-29 11:39:59 +0200

    [TASK] Add support for translate flash messages
    Change-Id: Ic531fd19b715419e2a8fdf97d142687cf455bd7e
    Resolves: #544

---
+++ Packages/Beech/Beech.Ehrm/Classes/Aspect/TranslateFlashMessageAspect.php
@@ -0,0 +1,36 @@
+<?php
+namespace Beech\Ehrm\Aspect;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Developer: Karol Kamiński <karol@beech.it>
+ * Date: 29-08-12 11:07
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Translation for flash message
+ *
+ * @FLOW3\Aspect
+ */
+class TranslateFlashMessageAspect {
+
+	/**
+	 * @var \TYPO3\FLOW3\I18n\Translator
+	 * @FLOW3\Inject
+	 */
+	protected $translator;
+
+	/**
+	 * @param \TYPO3\FLOW3\AOP\JoinPointInterface $joinPoint
+	 * @FLOW3\Before("method(Beech\.*\Controller\.*->addFlashMessage())")
+	 * @return void
+	 */
+	public function translateFlashMessageOnAdd(\TYPO3\FLOW3\AOP\JoinPointInterface $joinPoint) {
+		$joinPoint->setMethodArgument('messageBody', $this->translator->translateByOriginalLabel($joinPoint->getMethodArgument('messageBody')));
+	}
+}
+?>
