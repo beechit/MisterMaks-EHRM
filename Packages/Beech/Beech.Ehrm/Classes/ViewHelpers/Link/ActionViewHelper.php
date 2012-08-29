f98d03ed7b8a645fb9f6d745e45bfcbc9c0d6061
karol <karol@beech.it>
2012-08-29 16:27:26 +0200

    [BUGFIX] Fix bug about creating task with nonexisting urls
    Change-Id: Iab256b5a80b13a6ba657a0ecad5567102b1b1cfc
    Resolves: #560

---
+++ Packages/Beech/Beech.Ehrm/Classes/ViewHelpers/Link/ActionViewHelper.php
@@ -0,0 +1,60 @@
+<?php
+namespace Beech\Ehrm\ViewHelpers\Link;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 29-08-12 12:19
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * A view helper for creating links to actions.
+ * = Examples =
+ * <code title="Defaults">
+ * <f:link.action>some link</f:link.action>
+ * </code>
+ * <output>
+ * <a href="currentpackage/currentcontroller">some link</a>
+ * (depending on routing setup and current package/controller/action)
+ * </output>
+ * <code title="Additional arguments">
+ * <f:link.action action="myAction" controller="MyController" package="YourCompanyName.MyPackage" subpackage="YourCompanyName.MySubpackage" arguments="{key1: 'value1', key2: 'value2'}">some link</f:link.action>
+ * </code>
+ * <output>
+ * <a href="mypackage/mycontroller/mysubpackage/myaction?key1=value1&amp;key2=value2">some link</a>
+ * (depending on routing setup)
+ * </output>
+ *
+ * @api
+ */
+class ActionViewHelper extends \TYPO3\Fluid\ViewHelpers\Link\ActionViewHelper {
+
+	/**
+	 * Render the link.
+	 *
+	 * @param string $action Target action
+	 * @param array $arguments Arguments
+	 * @param string $controller Target controller. If NULL current controllerName is used
+	 * @param string $package Target package. if NULL current package is used
+	 * @param string $subpackage Target subpackage. if NULL current subpackage is used
+	 * @param string $section The anchor to be added to the URI
+	 * @param string $format The requested format, e.g. ".html"
+	 * @param array $additionalParams additional query parameters that won't be prefixed like $arguments (overrule $arguments)
+	 * @param boolean $addQueryString If set, the current query parameters will be kept in the URI
+	 * @param array $argumentsToBeExcludedFromQueryString arguments to be removed from the URI. Only active if $addQueryString = TRUE
+	 * @return string The rendered link
+	 * @api
+	 */
+	public function render($action = NULL, $arguments = array(), $controller = NULL, $package = NULL, $subpackage = NULL, $section = '', $format = '', array $additionalParams = array(), $addQueryString = FALSE, array $argumentsToBeExcludedFromQueryString = array()) {
+		try {
+			$output = parent::render($action, $arguments, $controller, $package, $subpackage, $section, $format, $additionalParams, $addQueryString, $argumentsToBeExcludedFromQueryString);
+			return $output;
+		} catch (\TYPO3\Fluid\Core\ViewHelper\Exception $exception) {
+			return $this->renderChildren();
+		}
+	}
+}
+
+?>
