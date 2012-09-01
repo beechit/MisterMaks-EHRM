f2275e4e903af576675b98d1cd20b47666855f24
Rens Admiraal <rens@beech.it>
2012-09-02 01:15:32 +0200

    [FEATURE] Setup controller
    Change-Id: I00eff722187e0a137e60e189b868b406d685e6e3
    Resolves: #577

---
+++ Packages/Beech/Beech.Ehrm/Classes/Controller/SetupController.php
@@ -0,0 +1,30 @@
+<?php
+namespace Beech\Ehrm\Controller;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 02-09-12 01:16
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Setup controller for the Beech.Ehrm package
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class SetupController extends AbstractController {
+
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+
+	}
+
+}
+
+?>
