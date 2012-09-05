726442f433919b0eade82461d16196b650af0948
karol <karol@beech.it>
2012-09-05 16:04:48 +0200

    [TASK] Assign translation based on username
    Create loading language from settings depends on username
    
    Change-Id: Iefe1aed52f189f4d34ce0aed0bfd9b3ac091e7e3
    Resolves: #89

--- Packages/Beech/Beech.Ehrm/Classes/Aspect/LocaleAspect.php
+++ Packages/Beech/Beech.Ehrm/Classes/Aspect/LocaleAspect.php
@@ -24,11 +24,40 @@ class LocaleAspect {
 	protected $frontendSession;
 
 	/**
+	 * @var \TYPO3\FLOW3\Object\ObjectManagerInterface
+	 * @FLOW3\Inject
+	 */
+	protected $objectManager;
+
+	/**
+	 * @var \TYPO3\FLOW3\Security\Context
+	 * @FLOW3\Inject
+	 */
+	protected $securityContext;
+
+	/**
+	 * Setting default locale to session
+	 *
+	 * To overwrite default language add user name to settings:
+	 * 	LanguageSetter:
+	 * 	  user:
+	 * 	    john: en
+	 * 	    bram: nl
+	 * 	    karol: pl
+	 *
 	 * @FLOW3\Before("method(TYPO3\Fluid\ViewHelpers\TranslateViewHelper->render())")
 	 * @param \TYPO3\FLOW3\AOP\JoinPointInterface $joinPoint The current join point
 	 * @return void
 	 */
 	public function addSessionLocaleToTranslateViewHelper(\TYPO3\FLOW3\AOP\JoinPointInterface $joinPoint) {
+		$account = $this->securityContext->getAccount();
+		if ($account instanceof \TYPO3\FLOW3\Security\Account) {
+			$accountIdentifier = $this->securityContext->getAccount()->getAccountIdentifier();
+			$userLanguageSetterCollection = $this->objectManager->getSettingsByPath(array('LanguageSetter', 'user'));
+			if (isset($userLanguageSetterCollection[$accountIdentifier])) {
+				$this->frontendSession->setCurrentLocale($userLanguageSetterCollection[$accountIdentifier]);
+			}
+		}
 		if (!$this->frontendSession->isInitialized()) {
 			$this->frontendSession->initialize();
 		}
