bd3b0a3193cc0f349a24523d31a6fb13450251e2
karol <karol@beech.it>
2012-09-13 12:15:26 +0200

    [TASK] Replace iframe in wizard, with ajax form.
    Refresh page behind modal box after form submit.
    Create new route for wizard
    Implement jquery.form plugin.
    
    Change-Id: Ice5849f092fd5c6252b2ff67335c3b67144c2af5
    Resolves: #594
    Resolves: #622

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/UserInterface.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/UserInterface.js
@@ -1,47 +1,103 @@
-define(['jquery', 'emberjs'], function(jQuery, Ember) {
+define(['jquery', 'emberjs', 'form'], function (jQuery, Ember, Form) {
 	return {
-		navigation: function() {
+		navigation: function () {
 			return Ember.Object.create({
-				initialize: function() {
+				initialize: function () {
 				}
 			});
 		},
 
-		modal: function() {
+		modal: function () {
+			var _self;
 			return Ember.Object.create({
-				initialize: function() {
-					var _self = this;
-					var $modal = jQuery('<div></div>').addClass('modal fade').attr('id', 'entityModal');
-					jQuery('section.container').append($modal);
-					jQuery('a[data-toggle=modal]').live('click', function() {
+				modalId: '',
+				initialize: function () {
+					_self = this;
+					_self.createModalBox();
+					jQuery('a[data-toggle=modal]').live('click', function () {
 						var target = jQuery(this).attr('data-target');
 						var url = jQuery(this).attr('href');
-						var content = jQuery('<iframe></iframe>').addClass('modal-body-content').attr('src',url);
+						var modal = _self.createModalBox(target);
 						jQuery(target)
 							.html(jQuery('.loading').html()) // Set default loading content
-							.find('.modal-body')
-							.html(content);
-						$(content).load(function() {
-							_self.contentLoadFinisher(this)
-						});
+						jQuery.ajax({
+							type: "POST",
+							url: url,
+							data: {}
+						}).done(function (response) {
+								_self.contentLoadFinisher(jQuery(target), response);
+							});
 					});
 				},
 
-				getContentTitle: function() {
-					return $('.modal-body-content').contents().find('legend').html();
+				process: function (response, statusText, xhr, $form) {
+					var target = '#' + jQuery($form).parents('.modal').attr('id');
+					_self.contentLoadFinisher(jQuery(target), response);
 				},
 
-				contentLoadFinisher: function(iframe) {
-					var title = this.getContentTitle();
-					var modal = $(iframe).parents('.modal');
-					modal.find('.modal-body-content').contents().find('legend').hide();
-					this.setModalTitle(modal, title);
+				isCreated: function (modalId) {
+					return (jQuery(modalId).length > 0) ? true : false;
 				},
 
-				setModalTitle: function(modal, title) {
+				createModalBox: function (modalId) {
+					if (modalId === undefined) {
+						modalId = this.getModalId();
+					}
+					if (!this.isCreated(modalId)) {
+						var modal = jQuery('<div></div>')
+							.addClass('modal fade')
+							.attr('id', modalId.replace('#', ''));
+						jQuery('section.container').append(modal);
+					}
+					return modal;
+				},
+
+				getModalId: function () {
+					if (this.get('modalId') === '') {
+						this.set('modalId', 'entityModal')
+					}
+					return this.get('modalId');
+				},
+
+				getContentTitle: function () {
+					return jQuery('#' + this.getModalId()).find('legend').html();
+				},
+
+				contentLoadFinisher: function (modal, content) {
+					if (this.hasRedirection(content)) {
+						var urlRegex = /([\d+];url=)((https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/;
+						var url = jQuery('<div></div>').append(content).find('meta[http-equiv="refresh"]').attr('content').replace(urlRegex, '$2');
+						if (url !== '') {
+							window.location = url;
+						}
+					} else {
+						// Set content for modal box body
+						modal.find('.modal-body')
+							.hide()// Hide until processing is finished
+							.html(content);
+						// Set title for modal box
+						var title = this.getContentTitle();
+						modal.find('legend').hide();
+						this.setModalTitle(modal, title);
+						// Set as ajax form
+						modal.find('form').ajaxForm({success: _self.process});
+						// Processing is finished, so show content
+						modal.find('.modal-body').show();
+					}
+				},
+
+				setModalTitle: function (modal, title) {
 					var titleContainer = modal.find('.modal-header h3');
 					titleContainer.html(title);
 					return;
+				},
+
+				hasRedirection: function (content) {
+					if (jQuery('<div></div>').append(content).find('meta[http-equiv="refresh"]').length === 1) {
+						return true;
+					} else {
+						return false;
+					}
 				}
 
 			});
