f6aaa88054ee7e50c1c820e75aa6558adbe5427c
Rens Admiraal <rens@beech.it>
2012-09-18 23:36:34 +0200

    [TASK] Kickstart Beech.Document package
    Change-Id: I2f90c9518add3b4c6ec5a861a07e9e7bfcc253ac
    Resolves: #654

---
+++ Packages/Beech/Beech.Document/Classes/Controller/StandardController.php
@@ -0,0 +1,29 @@
+<?php
+namespace Beech\Document\Controller;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 18-09-12 23:30
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Standard controller for the Beech.Document package
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class StandardController extends \Beech\Ehrm\Controller\AbstractController {
+
+	/**
+	 * Index action
+	 *
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+
+}
+
+?>
---
+++ Packages/Beech/Beech.Document/Classes/Package.php
@@ -0,0 +1,21 @@
+<?php
+namespace Beech\Document;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 18-09-12 23:30
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\FLOW3\Package\Package as BasePackage;
+use TYPO3\FLOW3\Annotations as FLOW3;
+
+/**
+ * Package base class of the Beech.Document package.
+ *
+ * @FLOW3\Scope("singleton")
+ */
+class Package extends BasePackage {
+
+}
+?>
