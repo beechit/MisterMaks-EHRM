a84f19f2580e7c046032dfddf0aee22cbe4bc0a2
karol <karol@beech.it>
2012-09-26 13:24:24 +0200

    [TASK] Small refactoring for Company and TaxData unit tests
    Change-Id: I9a3cd9087ee060916e91c405e8c0efb8a826d6f8
    Resolves: #676

--- Packages/Beech/Beech.Party/Tests/PHP/Unit/Domain/Model/Company/TaxDataTest.php
+++ Packages/Beech/Beech.Party/Tests/PHP/Unit/Domain/Model/Company/TaxDataTest.php
@@ -7,21 +7,45 @@ namespace Beech\Party\Tests\Unit\Domain\Model\Company;
 class TaxDataTest extends \TYPO3\Flow\Tests\UnitTestCase {
 
 	/**
-	 * @test
+	 * @var \Beech\Party\Domain\Model\Company\TaxData
 	 */
-	public function checkGetAndSetVatNumber() {
-		$taxData = new \Beech\Party\Domain\Model\Company\TaxData();
-		$taxData->setVatNumber('123123');
-		$this->assertEquals('123123', $taxData->getVatNumber());
+	protected $taxData;
+
+	/**
+	 * Set up for tests
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->taxData = new \Beech\Party\Domain\Model\Company\TaxData();
 	}
 
 	/**
+	 * @dataProvider taxDataDataProvider
 	 * @test
 	 */
-	public function checkGetAndSetWageNumber() {
-		$taxData = new \Beech\Party\Domain\Model\Company\TaxData();
-		$taxData->setWageTaxNumber('111222');
-		$this->assertEquals('111222', $taxData->getWageTaxNumber());
+	public function checkGetAndSetVatNumber($wageTaxNumber, $vatNumber) {
+		$this->taxData->setVatNumber($vatNumber);
+		$this->assertEquals($vatNumber, $this->taxData->getVatNumber());
+	}
+
+	/**
+	 * @dataProvider taxDataDataProvider
+	 * @test
+	 */
+	public function checkGetAndSetWageNumber($wageTaxNumber, $vatNumber) {
+		$this->taxData->setWageTaxNumber($wageTaxNumber);
+		$this->assertEquals($wageTaxNumber, $this->taxData->getWageTaxNumber());
+	}
+
+	/**
+	 * @return array Signature: wageTaxNumber, vatNumber
+	 */
+	public function taxDataDataProvider() {
+		return array(
+			array('212121212', '534543543'),
+			array('412121222', '2232322'),
+			array('544543454', '756565656')
+		);
 	}
 }
 
--- Packages/Beech/Beech.Party/Tests/PHP/Unit/Domain/Model/CompanyTest.php
+++ Packages/Beech/Beech.Party/Tests/PHP/Unit/Domain/Model/CompanyTest.php
@@ -12,187 +12,183 @@ use \TYPO3\Party\Domain\Model\PersonName;
 class CompanyTest extends \TYPO3\Flow\Tests\UnitTestCase {
 
 	/**
+	 * @var \Beech\Party\Domain\Model\Company
+	 */
+	protected $company;
+
+	/**
+	 * Set up for tests
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->company = new Company();
+	}
+
+	/**
+	 * @dataProvider companyDataProvider
 	 * @test
 	 */
-	public function settingSimpleProperties() {
-		$company = new Company();
-		$company->setName($this->getData(0, 0));
-		$company->setCompanyNumber($this->getData(0, 1));
-		$company->setCompanyType($this->getData(0, 2));
-		$company->setDescription($this->getData(0, 3));
-		$company->setLegalForm($this->getData(0, 4));
-		$company->setChamberOfCommerceNumber($this->getData(0, 5));
-		$this->assertSame($company->getName(), $this->getData(0, 0));
-		$this->assertSame($company->getCompanyNumber(), $this->getData(0, 1));
-		$this->assertSame($company->getCompanyType(), $this->getData(0, 2));
-		$this->assertSame($company->getDescription(), $this->getData(0, 3));
-		$this->assertSame($company->getLegalForm(), $this->getData(0, 4));
-		$this->assertSame($company->getChamberOfCommerceNumber(), $this->getData(0, 5));
+	public function settingSimpleProperties($name, $companyNumber, $companyType, $description, $legalForm, $chamberOfCommerceNumber) {
+		$this->company->setName($name);
+		$this->company->setCompanyNumber($companyNumber);
+		$this->company->setCompanyType($companyType);
+		$this->company->setDescription($description);
+		$this->company->setLegalForm($legalForm);
+		$this->company->setChamberOfCommerceNumber($chamberOfCommerceNumber);
+		$this->assertSame($this->company->getName(), $name);
+		$this->assertSame($this->company->getCompanyNumber(), $companyNumber);
+		$this->assertSame($this->company->getCompanyType(), $companyType);
+		$this->assertSame($this->company->getDescription(), $description);
+		$this->assertSame($this->company->getLegalForm(), $legalForm);
+		$this->assertSame($this->company->getChamberOfCommerceNumber(), $chamberOfCommerceNumber);
 	}
 
 	/**
 	 * @test
 	 */
 	public function addTaxData() {
-		$company = new Company();
 			// check if company has no tax data
-		$this->assertNull($company->getTaxData());
+		$this->assertNull($this->company->getTaxData());
 			// check if company has tax data
 		$taxData = new \Beech\Party\Domain\Model\Company\TaxData();
-		$company->setTaxData($taxData);
-		$this->assertNotNull($company->getTaxData());
+		$this->company->setTaxData($taxData);
+		$this->assertNotNull($this->company->getTaxData());
 	}
 
 	/**
 	 * @test
 	 */
 	public function addAndRemoveDepartments() {
-		$company = new Company();
 			// check if company has no departments
-		$this->assertEquals(0, $company->getDepartments()->count());
+		$this->assertEquals(0, $this->company->getDepartments()->count());
 			// create first department
 		$oneDepartment = new Company();
 		$oneDepartment->setName('Department 1');
-		$company->addDepartment($oneDepartment);
+		$this->company->addDepartment($oneDepartment);
 			// check if company has one department
-		$this->assertEquals(1, $company->getDepartments()->count());
+		$this->assertEquals(1, $this->company->getDepartments()->count());
 			// create second department
 		$secondDepartment = new Company();
 		$secondDepartment->setName('Department 2');
-		$company->addDepartment($secondDepartment);
+		$this->company->addDepartment($secondDepartment);
 			// check if company has two departments
-		$this->assertEquals(2, $company->getDepartments()->count());
-		$company->removeDepartment($oneDepartment);
-		$company->removeDepartment($secondDepartment);
+		$this->assertEquals(2, $this->company->getDepartments()->count());
+		$this->company->removeDepartment($oneDepartment);
+		$this->company->removeDepartment($secondDepartment);
 			// check if departments were removed
-		$this->assertEquals(0, $company->getDepartments()->count());
+		$this->assertEquals(0, $this->company->getDepartments()->count());
 	}
 
 	/**
 	 * @test
 	 */
 	public function addAndRemoveAddresses() {
-		$company = new Company();
 			// check if company has no address
-		$this->assertEquals(0, $company->getAddresses()->count());
+		$this->assertEquals(0, $this->company->getAddresses()->count());
 			// create first department
 		$address = new \Beech\Party\Domain\Model\Address();
-		$company->addAddress($address);
+		$this->company->addAddress($address);
 			// check if company has address
-		$this->assertEquals(1, $company->getAddresses()->count());
-		$company->removeAddress($address);
+		$this->assertEquals(1, $this->company->getAddresses()->count());
+		$this->company->removeAddress($address);
 			// check if address were removed
-		$this->assertEquals(0, $company->getAddresses()->count());
+		$this->assertEquals(0, $this->company->getAddresses()->count());
 	}
 
 	/**
 	 * @test
 	 */
 	public function addAndRemoveElectronicAddresses() {
-		$company = new Company();
 			// check if company has no electronic address
-		$this->assertCount(0, $company->getElectronicAddresses());
+		$this->assertCount(0, $this->company->getElectronicAddresses());
 			// add email
 		$email = new ElectronicAddress();
 		$email->setType(ElectronicAddress::TYPE_EMAIL);
-		$company->setPrimaryElectronicAddress($email);
+		$this->company->setPrimaryElectronicAddress($email);
 			// check if company has got electronic address
-		$this->assertCount(1, $company->getElectronicAddresses());
+		$this->assertCount(1, $this->company->getElectronicAddresses());
 			// check if its email
-		$this->assertEquals(ElectronicAddress::TYPE_EMAIL, $company->getElectronicAddresses()->get(0)->getType());
+		$this->assertEquals(ElectronicAddress::TYPE_EMAIL, $this->company->getElectronicAddresses()->get(0)->getType());
 			// add phone
 		$phone = new ElectronicAddress();
 		$phone->setType(ElectronicAddress::TYPE_PHONE);
-		$company->addElectronicAddress($phone);
+		$this->company->addElectronicAddress($phone);
 			// check if company has got two electronic addresses
-		$this->assertCount(2, $company->getElectronicAddresses());
+		$this->assertCount(2, $this->company->getElectronicAddresses());
 			// check if its phone
-		$this->assertEquals(ElectronicAddress::TYPE_PHONE, $company->getElectronicAddresses()->get(1)->getType());
+		$this->assertEquals(ElectronicAddress::TYPE_PHONE, $this->company->getElectronicAddresses()->get(1)->getType());
 			// check if primary electronic address is email
-		$this->assertEquals(ElectronicAddress::TYPE_EMAIL, $company->getPrimaryElectronicAddress()->getType());
+		$this->assertEquals(ElectronicAddress::TYPE_EMAIL, $this->company->getPrimaryElectronicAddress()->getType());
 			// removing data
-		$company->removeElectronicAddress($email);
-		$company->removeElectronicAddress($phone);
-		$this->assertCount(0, $company->getElectronicAddresses());
+		$this->company->removeElectronicAddress($email);
+		$this->company->removeElectronicAddress($phone);
+		$this->assertCount(0, $this->company->getElectronicAddresses());
 	}
 
 	/**
 	 * @test
 	 */
 	public function setAsDeleted() {
-		$company = new Company();
 			// check if its not set as deleted
-		$this->assertFalse($company->getDeleted());
+		$this->assertFalse($this->company->getDeleted());
 			// check if its set as deleted
-		$company->setDeleted(TRUE);
-		$this->assertTrue($company->getDeleted());
+		$this->company->setDeleted(TRUE);
+		$this->assertTrue($this->company->getDeleted());
 	}
 
 	/**
 	 * @test
 	 */
 	public function checkParentCompanyOfDepartment() {
-		$company = new Company();
-		$company->setName('Company');
 		$department = new Company();
 		$department->setName('Department');
-		$company->addDepartment($department);
-		$this->assertEquals($company, $department->getParentCompany());
+		$this->company->addDepartment($department);
+		$this->assertEquals($this->company, $department->getParentCompany());
 	}
 
 	/**
 	 * @test
 	 */
 	public function addAndRemoveContactPerson() {
-		$company = new Company();
 		$person = new Person();
 		$person->addPersonName(new PersonName('Mr', 'John', '', 'Matrix'));
-		$this->assertCount(0, $company->getContactPersons());
-		$company->addContactPerson($person);
-		$this->assertCount(1, $company->getContactPersons());
+		$this->assertCount(0, $this->company->getContactPersons());
+		$this->company->addContactPerson($person);
+		$this->assertCount(1, $this->company->getContactPersons());
 			// is it really the same person ?
-		$this->assertEquals($person, $company->getContactPersons()->get(0));
+		$this->assertEquals($person, $this->company->getContactPersons()->get(0));
 			// are you sure ?
-		$this->assertEquals('Mr John Matrix', $company->getContactPersons()->get(0)->getName()->getFullName());
+		$this->assertEquals('Mr John Matrix', $this->company->getContactPersons()->get(0)->getName()->getFullName());
 			// John you are fired!
-		$company->removeContactPerson($person);
-		$this->assertCount(0, $company->getContactPersons());
+		$this->company->removeContactPerson($person);
+		$this->assertCount(0, $this->company->getContactPersons());
 	}
 
 	/**
 	 * @test
 	 */
 	public function setCollectionOfContactPersons() {
-		$company = new Company();
 		$john = new Person();
 		$bob = new Person();
 		$emmy = new Person();
-		$company->addContactPerson($john);
-		$company->addContactPerson($bob);
-		$company->addContactPerson($emmy);
+		$this->company->addContactPerson($john);
+		$this->company->addContactPerson($bob);
+		$this->company->addContactPerson($emmy);
 		$newCompany = new Company();
 			// copy contact list...
-		$newCompany->setContactPersons($company->getContactPersons());
-		$this->assertEquals($company->getContactPersons(), $newCompany->getContactPersons());
+		$newCompany->setContactPersons($this->company->getContactPersons());
+		$this->assertEquals($this->company->getContactPersons(), $newCompany->getContactPersons());
 	}
 
 	/**
-	 * @return array
+	 * @return array Signature: name, companyNumber, companyType, description, legalForm, chamberOfCommerceNumber
 	 */
-	private function dataProvider() {
+	public function companyDataProvider() {
 		return array(
 			array('Beech.IT', '123', 'Type 1', 'Nice company', 'B.V.', '212121212'),
 			array('Emaux', '222', 'Type 1', 'Other company', 'B.V.', '412121222'),
 			array('Google Inc.', '444', 'Type 2', 'Big company', 'B.V.', '544543454'),
 		);
-	}
-
-	/**
-	 * @return string
-	 */
-	private function getData($row, $column) {
-		$testData = $this->dataProvider();
-		return $testData[$row][$column];
 	}
 
 }
