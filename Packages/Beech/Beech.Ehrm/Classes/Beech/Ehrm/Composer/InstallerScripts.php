b2a852b2b561804d2a7ab861afb622159b141b64
Rens Admiraal <rens@beech.it>
2012-09-26 21:49:29 +0200

    [TASK] Add after install / update script
    Change-Id: I535897c6a6163fa62eab32e1301f23937d78c4eb
    Related: #683

---
+++ Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/Composer/InstallerScripts.php
@@ -0,0 +1,23 @@
+<?php
+namespace Beech\Ehrm\Composer;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 26-09-12 21:33
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+class InstallerScripts extends \TYPO3\FLOW3\Composer\InstallerScripts {
+
+	/**
+	 * @param \Composer\Script\Event $event
+	 * @return void
+	 */
+	static public function postUpdateAndInstall(Event $event) {
+		require_once('../../../../../../../Build/Essentials/fetchQueuedPatches.php');
+		parent::postUpdateAndInstall($event);
+	}
+
+}
+
+?>
