e2f9c55400720b09ba107464cc9965609a44f8fa
Rens Admiraal <rens@beech.it>
2012-10-01 21:10:18 +0200

    [TASK] Add Buster.js configuration
    Change-Id: I90207c39cfe2ae137b1e2e332f262c4eb66ac1dc
    Resolves: #717

--- Build/JSUnit/buster.js
+++ Build/JSUnit/buster.js
@@ -3,12 +3,29 @@ var config = module.exports;
 config['Beech.Ehrm Unit'] = {
 	rootPath: '../../',
 	environment: 'browser',
-	tests: ['Packages/Application/Beech.Ehrm/Tests/JavaScript/Unit/**/*.js'],
+	tests: ['Packages/Application/**/Tests/JavaScript/Unit/**/*.js'],
+	extensions: [require('buster-lint')],
+	'buster-lint': {
+		linter: 'jshint',
+		linterOptions: {
+			eqeqeq: true,
+			eqnull: false,
+			nomen: false,
+			unused: true,
+			strict: true,
+			trailing: true
+		},
+		excludes: [
+			'.*/require.js',
+			'.*/Library/.*',
+			'.*/Tests/JavaScript/.*'
+		]
+	},
 	libs: [
 		'Packages/Application/Beech.Ehrm/Resources/Public/JavaScript/require.js'
 	],
 	resources: [
-		'Web/_Resources/Static/Packages/**/*.js'
+		'Web/_Resources/Static/Packages/Beech.*/JavaScript/**/*.js'
 	],
 	sources: [
 		'Packages/Application/Beech.Ehrm/Tests/JavaScript/Unit.js'
