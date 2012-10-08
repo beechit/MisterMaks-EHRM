0edd466c1246577b1f330e85378ce665fb1f484c
Rens Admiraal <rens@beech.it>
2012-10-08 14:58:52 +0200

    [TASK] Adapt code to Flow rename
    Change-Id: I8c10efecceb145e0d468e949cc5b6b099c9f5d87
    Related: #725

--- Packages/Beech/Beech.Task/Classes/Beech/Task/Controller/StandardController.php
+++ Packages/Beech/Beech.Task/Classes/Beech/Task/Controller/StandardController.php
@@ -7,12 +7,12 @@ namespace Beech\Task\Controller;
  * All code (c) Beech Applications B.V. all rights reserved
  */
 
-use TYPO3\FLOW3\Annotations as FLOW3;
+use TYPO3\Flow\Annotations as Flow;
 
 /**
  * Standard controller for the Beech.Task package
  *
- * @FLOW3\Scope("singleton")
+ * @Flow\Scope("singleton")
  */
 class StandardController extends \Beech\Ehrm\Controller\AbstractController {
 
