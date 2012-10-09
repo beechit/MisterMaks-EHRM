adc31301b479470aca29f4d54c09aa50c3d5150f
Rens Admiraal <rens@beech.it>
2012-10-09 13:14:15 +0200

    [TASK] Set git config for packages after composer run
    This change automatically sets the git commit-msg hook and push
    configuration. Besides that it's possible to set a user.email
    configuration for people who use no global setting for user.email.
    
    Usage:
    composer update --dev [email]
    
    This change only affects the dev switch.
    
    Resolves: #730
    Change-Id: Ibf8101950e7fece53bd62b0dcbc4cab21dd675a0

--- Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/Composer/InstallerScripts.php
+++ Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/Composer/InstallerScripts.php
@@ -20,6 +20,33 @@ class InstallerScripts extends \TYPO3\Flow\Composer\InstallerScripts {
 	 */
 	static public function postUpdateAndInstall(Event $event) {
 		require_once('Build/essentials/fetchQueuedPatches.php');
+
+		if (isset($_SERVER['argv'][2]) && $_SERVER['argv'][2] === '--dev') {
+			$mainDir = realpath(__DIR__ . '/../../../../../../../') . '/';
+
+			$packageFolders = glob('Packages/**');
+			foreach ($packageFolders as $packageFolder) {
+				$packages = glob($packageFolder . '/**');
+				foreach ($packages as $package) {
+					if (is_dir($package) && strpos($package, 'Beech.') !== FALSE) {
+						$commands = array(
+							'scp -p git.beech.local:hooks/commit-msg .git/hooks/',
+							'git config remote.origin.push HEAD:refs/for/development'
+						);
+
+						foreach ($_SERVER['argv'] as $arg) {
+							if (filter_var($arg, FILTER_VALIDATE_EMAIL)) {
+								$commands[] = 'git config user.email ' . $arg;
+							}
+						}
+
+						$cmd = sprintf('cd %s%s/ && ' . implode('&&', $commands), $mainDir, $package);
+						shell_exec($cmd);
+					}
+				}
+			}
+		}
+
 		parent::postUpdateAndInstall($event);
 	}
 
