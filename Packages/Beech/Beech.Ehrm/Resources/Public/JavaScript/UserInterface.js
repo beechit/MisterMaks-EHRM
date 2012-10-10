aa79353c61760b9b298898bfa054465f12746005
Rens Admiraal <rens@beech.it>
2012-10-10 12:50:40 +0200

    [TASK] Remove conflicting JS / CSS for adding the debug toolbar
    Change-Id: I1df5391728ada89aa3409787210b79a012f6c9c0
    Related: #734

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/UserInterface.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/UserInterface.js
@@ -1,4 +1,4 @@
-define(['jquery', 'emberjs', 'form'], function (jQuery, Ember) {
+define(['jquery', 'emberjs', 'form'], function ($, Ember) {
 	return {
 		navigation: function () {
 			return Ember.Object.create({
@@ -8,37 +8,36 @@ define(['jquery', 'emberjs', 'form'], function (jQuery, Ember) {
 		},
 
 		modal: function () {
-			var _self;
 			return Ember.Object.create({
 
 				modalId: '',
 
 				initialize: function () {
-					_self = this;
-					_self.createModalBox();
-					jQuery('a[data-toggle=modal]').live('click', function () {
-						var target = jQuery(this).attr('data-target');
-						var url = jQuery(this).attr('href');
-						var modal = _self.createModalBox(target);
-						jQuery(target)
-							.html(jQuery('.loading').html()) // Set default loading content
-						jQuery.ajax({
+					var that = this;
+					that.createModalBox();
+					$('a[data-toggle=modal]').live('click', function () {
+						var target = $(this).attr('data-target');
+						var url = $(this).attr('href');
+						var modal = that.createModalBox(target);
+						$(target)
+							.html($('.loading').html()) // Set default loading content
+						$.ajax({
 							type: 'POST',
 							url: url,
 							data: {}
 						}).done(function (response) {
-								_self.contentLoadFinisher(jQuery(target), response);
+								that.contentLoadFinisher($(target), response);
 							});
 					});
 				},
 
 				process: function (response, statusText, xhr, $form) {
-					var target = '#' + jQuery($form).parents('.modal').attr('id');
-					_self.contentLoadFinisher(jQuery(target), response);
+					var target = '#' + $($form).parents('.modal').attr('id');
+					that.contentLoadFinisher($(target), response);
 				},
 
 				isCreated: function (modalId) {
-					return (jQuery(modalId).length > 0) ? true : false;
+					return ($(modalId).length > 0) ? true : false;
 				},
 
 				createModalBox: function (modalId) {
@@ -46,10 +45,10 @@ define(['jquery', 'emberjs', 'form'], function (jQuery, Ember) {
 						modalId = this.getModalId();
 					}
 					if (!this.isCreated(modalId)) {
-						var modal = jQuery('<div></div>')
+						var modal = $('<div></div>')
 							.addClass('modal fade')
 							.attr('id', modalId.replace('#', ''));
-						jQuery('section.container').append(modal);
+						$('section.container').append(modal);
 					}
 					return modal;
 				},
@@ -62,13 +61,13 @@ define(['jquery', 'emberjs', 'form'], function (jQuery, Ember) {
 				},
 
 				getContentTitle: function () {
-					return jQuery('#' + this.getModalId()).find('legend').html();
+					return $('#' + this.getModalId()).find('legend').html();
 				},
 
 				contentLoadFinisher: function (modal, content) {
 					if (this.hasRedirection(content)) {
 						var urlRegex = /([\d+];url=)((https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?)/;
-						var url = jQuery('<div></div>').append(content).find('meta[http-equiv="refresh"]').attr('content').replace(urlRegex, '$2');
+						var url = $('<div></div>').append(content).find('meta[http-equiv="refresh"]').attr('content').replace(urlRegex, '$2');
 						if (url !== '') {
 							window.location = url;
 						}
@@ -82,7 +81,7 @@ define(['jquery', 'emberjs', 'form'], function (jQuery, Ember) {
 						modal.find('legend').hide();
 						this.setModalTitle(modal, title);
 						// Set as ajax form
-						modal.find('form').ajaxForm({success: _self.process});
+						modal.find('form').ajaxForm({success: that.process});
 						// Processing is finished, so show content
 						modal.find('.modal-body').show();
 					}
@@ -95,7 +94,7 @@ define(['jquery', 'emberjs', 'form'], function (jQuery, Ember) {
 				},
 
 				hasRedirection: function (content) {
-					if (jQuery('<div></div>').append(content).find('meta[http-equiv="refresh"]').length === 1) {
+					if ($('<div></div>').append(content).find('meta[http-equiv="refresh"]').length === 1) {
 						return true;
 					} else {
 						return false;
