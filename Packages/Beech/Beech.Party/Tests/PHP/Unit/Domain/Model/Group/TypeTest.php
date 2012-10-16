efc22c2d94a9db447fe5f13811598a02451a4d6f
karol <karol@beech.it>
2012-10-16 15:00:14 +0200

    [TASK] Create UI for managing grouptypes
    Change-Id: I32e8cae035d44b3ad2b73025ee225e5e6683d79c
    Resolves: #688

---
+++ Packages/Beech/Beech.Party/Tests/PHP/Unit/Domain/Model/Group/TypeTest.php
@@ -0,0 +1,44 @@
+<?php
+namespace Beech\Party\Tests\Unit\Domain\Model\Group;
+
+use \Beech\Party\Domain\Model\Group\Type as GroupType;
+
+/**
+ * Testcase for Group type
+ */
+class GroupTypeTest extends \TYPO3\Flow\Tests\UnitTestCase {
+
+	/**
+	 * @var \Beech\Party\Domain\Model\Group\Type
+	 */
+	protected $groupType;
+
+	/**
+	 * Set up for tests
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->groupType = new GroupType();
+	}
+
+	/**
+	 * @dataProvider groupTypeDataProvider
+	 * @test
+	 */
+	public function settingGroupName($name) {
+		$this->groupType->setName($name);
+		$this->assertSame($this->groupType->getName(), $name);
+	}
+
+	/**
+	 * @return array Signature: name
+	 */
+	public function groupTypeDataProvider() {
+		return array(
+			array('System users'),
+			array('Guests'),
+			array('Other Type 123')
+		);
+	}
+}
+?>
