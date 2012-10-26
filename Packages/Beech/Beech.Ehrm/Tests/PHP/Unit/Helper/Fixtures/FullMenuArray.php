fe4d229cb81e731810a67e48b3bdf7de33427033
Rens Admiraal <rens@beech.it>
2012-10-26 17:15:16 +0200

    [BUGFIX] Fix failing unit test in SettingsHelper
    Change-Id: I993f6007f043d19c02dc1b983120dd87829c2711
    Related: #842

--- Packages/Beech/Beech.Ehrm/Tests/PHP/Unit/Helper/Fixtures/FullMenuArray.php
+++ Packages/Beech/Beech.Ehrm/Tests/PHP/Unit/Helper/Fixtures/FullMenuArray.php
@@ -5,6 +5,7 @@ return array(
 		'items' => array(
 			0 => array(
 				'label' => 'Test',
+				'menuGroup' => 'main:new',
 				'href' => 'beech.ehrm/standard/index',
 			)
 		)
@@ -14,10 +15,12 @@ return array(
 		'items' => array(
 			0 => array(
 				'label' => 'Admin index',
+				'menuGroup' => 'main:manage',
 				'href' => 'beech.ehrm/standard/index',
 			),
 			1 => array(
 				'label' => 'Admin index',
+				'menuGroup' => 'main:manage',
 				'href' => 'beech.ehrm/standard/index',
 			)
 		)
