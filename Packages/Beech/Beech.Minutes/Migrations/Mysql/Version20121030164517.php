7de7521302d2ce8433f9f5b6f03a2835ffe91a2c
Rens Admiraal <rens@beech.it>
2012-10-30 16:57:13 +0100

    [TASK] Update MySQL migrations and dependencies
    Change-Id: I81e61e719f255100acc7a4c8e25c5119b13d9818
    Related: #850

--- Packages/Beech/Beech.Minutes/Migrations/Mysql/Version20121030164517.php
+++ Packages/Beech/Beech.Minutes/Migrations/Mysql/Version20121030164517.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 
 /**
  */
-class Version20121017105238 extends AbstractMigration {
+class Version20121030164517 extends AbstractMigration {
 
 	/**
 	 * @param Schema $schema
@@ -15,7 +15,6 @@ class Version20121017105238 extends AbstractMigration {
 	public function up(Schema $schema) {
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
 
-		$this->addSql("CREATE TABLE beech_minutes_domain_model_minute (persistence_object_identifier VARCHAR(40) NOT NULL, personsubject VARCHAR(40) DEFAULT NULL, personinitiator VARCHAR(40) DEFAULT NULL, title VARCHAR(255) NOT NULL, minutetype VARCHAR(255) DEFAULT NULL, content VARCHAR(255) DEFAULT NULL, creationdatetime DATETIME NOT NULL, INDEX IDX_42CEA5FD9E58E94F (personsubject), INDEX IDX_42CEA5FDFF9D15C0 (personinitiator), PRIMARY KEY(persistence_object_identifier)) ENGINE = InnoDB");
 		$this->addSql("ALTER TABLE beech_minutes_domain_model_minute ADD CONSTRAINT FK_42CEA5FD9E58E94F FOREIGN KEY (personsubject) REFERENCES beech_party_domain_model_person (persistence_object_identifier)");
 		$this->addSql("ALTER TABLE beech_minutes_domain_model_minute ADD CONSTRAINT FK_42CEA5FDFF9D15C0 FOREIGN KEY (personinitiator) REFERENCES beech_party_domain_model_person (persistence_object_identifier)");
 	}
@@ -27,7 +26,8 @@ class Version20121017105238 extends AbstractMigration {
 	public function down(Schema $schema) {
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
 
-		$this->addSql("DROP TABLE beech_minutes_domain_model_minute");
+		$this->addSql("ALTER TABLE beech_minutes_domain_model_minute DROP FOREIGN KEY FK_42CEA5FD9E58E94F");
+		$this->addSql("ALTER TABLE beech_minutes_domain_model_minute DROP FOREIGN KEY FK_42CEA5FDFF9D15C0");
 	}
 }
 
