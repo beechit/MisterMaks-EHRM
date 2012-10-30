e59faa644c42f8dff662389ff94627e2fbf3cd37
Rens Admiraal <rens@beech.it>
2012-10-30 16:56:07 +0100

    [TASK] Update MySQL migrations and dependencies
    Change-Id: I1e21a7520411d5b39a082cf6c3587c8d08acaa3c
    Related: #850

--- Packages/Beech/Beech.Calendar/Migrations/Mysql/Version20121030163933.php
+++ Packages/Beech/Beech.Calendar/Migrations/Mysql/Version20121030163933.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 
 /**
  */
-class Version20121018103649 extends AbstractMigration {
+class Version20121030163933 extends AbstractMigration {
 
 	/**
 	 * @param Schema $schema
@@ -15,8 +15,6 @@ class Version20121018103649 extends AbstractMigration {
 	public function up(Schema $schema) {
 		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
 
-		$this->addSql("CREATE TABLE beech_calendar_domain_model_meeting (persistence_object_identifier VARCHAR(40) NOT NULL, subject VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, startdatetime DATETIME NOT NULL, enddatetime DATETIME NOT NULL, PRIMARY KEY(persistence_object_identifier)) ENGINE = InnoDB");
-		$this->addSql("CREATE TABLE beech_calendar_domain_model_meeting_attendees_join (calendar_meeting VARCHAR(40) NOT NULL, party_person VARCHAR(40) NOT NULL, INDEX IDX_5D3C3E1397629AFE (calendar_meeting), INDEX IDX_5D3C3E1372AAAA2F (party_person), PRIMARY KEY(calendar_meeting, party_person)) ENGINE = InnoDB");
 		$this->addSql("ALTER TABLE beech_calendar_domain_model_meeting_attendees_join ADD CONSTRAINT FK_5D3C3E1397629AFE FOREIGN KEY (calendar_meeting) REFERENCES beech_calendar_domain_model_meeting (persistence_object_identifier)");
 		$this->addSql("ALTER TABLE beech_calendar_domain_model_meeting_attendees_join ADD CONSTRAINT FK_5D3C3E1372AAAA2F FOREIGN KEY (party_person) REFERENCES beech_party_domain_model_person (persistence_object_identifier)");
 	}
@@ -30,8 +28,7 @@ class Version20121018103649 extends AbstractMigration {
 
 		$this->addSql("ALTER TABLE beech_calendar_domain_model_meeting_attendees_join DROP FOREIGN KEY FK_5D3C3E1397629AFE");
 		$this->addSql("ALTER TABLE beech_calendar_domain_model_meeting_attendees_join DROP FOREIGN KEY FK_5D3C3E1372AAAA2F");
-		$this->addSql("DROP TABLE beech_calendar_domain_model_meeting");
-		$this->addSql("DROP TABLE beech_calendar_domain_model_meeting_attendees_join");
 	}
 }
+
 ?>
