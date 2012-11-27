b3b85b87fdf3e18819da4a4d135d865da2a5d7df
Henjo Hoeksma <hphoeksma@stylence.nl>
2012-11-27 15:44:33 +0100

    [TASK] Some design improvements
    Change-Id: I7fc5f69ac915f67fb6c4aa4431097456eeffc88c
    Related: #893

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/AllTodos.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/AllTodos.js
@@ -4,6 +4,7 @@ define (
 		MM.init.preInitialize.push(function() {
 			App.AllTodosController = Ember.ArrayController.extend();
 			App.AllTodosView = Ember.View.extend({
+				classNames: ['well'],
 				templateName: 'alltodos'
 			});
 		});
--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/DashboardWidget.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/DashboardWidget.js
@@ -4,6 +4,7 @@ define (
 		MM.init.preInitialize.push(function() {
 			App.DashboardWidgetController = Ember.Controller.extend();
 			App.DashboardWidgetView = Ember.View.extend({
+				classNames: ['span10'],
 				templateName: 'dashboardwidget'
 			});
 		});
