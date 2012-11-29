eae1cdd0af6a6455809869f3eda76463218f11c9
Rens Admiraal <rens@beech.it>
2012-11-29 14:43:45 +0100

    [TASK] Update models to CouchDB
    Change-Id: I30809a02073b26ae9b737cb10b72471f2a02c96c
    Related: #884

---
+++ Packages/Beech/Beech.Party/Classes/Beech/Party/Domain/Repository/PersonDocumentRepository.php
@@ -0,0 +1,19 @@
+<?php
+namespace Beech\Party\Domain\Repository;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 29-11-12 14:24
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class PersonDocumentRepository extends \Radmiraal\CouchDB\Persistence\AbstractRepository {
+
+}
+
+?>
