2f0b818eb821e209517d8e7f7a8d4f1eaa88f62e
Rens Admiraal <rens@beech.it>
2012-11-29 20:23:46 +0100

    [TASK] Add MySQL / Postgresql database migrations
    Change-Id: I67792faa61e5bca68df1a6f77da03661a4b77e52
    Related: #884

---
+++ Packages/Beech/Beech.Ehrm/Migrations/Mysql/Version20121129201401.php
@@ -0,0 +1,33 @@
+<?php
+namespace TYPO3\Flow\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+	Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add application model
+ */
+class Version20121129201401 extends AbstractMigration {
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function up(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("CREATE TABLE beech_ehrm_domain_model_application (persistence_object_identifier VARCHAR(40) NOT NULL, company VARCHAR(40) DEFAULT NULL, UNIQUE INDEX UNIQ_5D373F1F4FBF094F (company), PRIMARY KEY(persistence_object_identifier)) ENGINE = InnoDB");
+	}
+
+	/**
+	 * @param Schema $schema
+	 * @return void
+	 */
+	public function down(Schema $schema) {
+		$this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql");
+
+		$this->addSql("DROP TABLE beech_ehrm_domain_model_application");
+	}
+}
+
+?>
