d285813dc193b70fa7e9fb099846a45f58447e86
karol <karol@beech.it>
2013-01-15 10:00:10 +0100

    [FEATURE] Add datepicker library
    Add javascript library to support picking up a date from
    calendar popup.
    
    Change-Id: I5218bd8b99078210d6dc22483fe0e2af31337baa

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Application.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Application.js
@@ -3,6 +3,16 @@
 	App.ApplicationController = App.ApplicationController.reopen({
 		title: 'Mister Maks',
 
+		init: function() {
+			$('.datepicker').live('click', function() {
+				$(this).datepicker({showOn:'focus'})
+					.on('changeDate', function() {
+						$(this).datepicker('hide')
+					})
+					.focus();
+			});
+		},
+
 		ready: function() {
 			$('[rel=tooltip]').tooltip();
 			$('[rel=popover]').popover();
