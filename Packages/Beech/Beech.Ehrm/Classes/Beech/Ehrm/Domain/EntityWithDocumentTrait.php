fe54461ee4c9b154f742c0a7529901df7f67c527
Rens Admiraal <rens@beech.it>
2013-02-13 15:44:38 +0100

    [TASK] Move shared methods to trait
    This change moves some shared methods for having DocumentWithEntity
    to a trait.
    
    Change-Id: Ib13ff40ed4e2b2aaa00c763868417a7396b29b08

---
+++ Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/Domain/EntityWithDocumentTrait.php
@@ -0,0 +1,139 @@
+<?php
+namespace Beech\Ehrm\Domain;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 2/13/13 1:42 PM
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * Trait which can be used to add automatic storage of
+ * unknown class properties into a related CouchDB document.
+ */
+trait EntityWithDocumentTrait {
+
+	/**
+	 * @var string
+	 */
+	protected $document = '';
+
+	/**
+	 * @var \Beech\Ehrm\Domain\Model\Document
+	 * @Flow\Transient
+	 */
+	protected $documentObject;
+
+	/**
+	 * @var \Doctrine\ODM\CouchDB\DocumentManager
+	 * @Flow\Transient
+	 */
+	protected $documentManager;
+
+	/**
+	 * @var \Radmiraal\CouchDB\Persistence\DocumentManagerFactory
+	 * @Flow\Transient
+	 */
+	protected $documentManagementFactory;
+
+	/**
+	 * @param \Radmiraal\CouchDB\Persistence\DocumentManagerFactory $documentManagerFactory
+	 * @return void
+	 */
+	public function injectDocumentManagerFactory(\Radmiraal\CouchDB\Persistence\DocumentManagerFactory $documentManagerFactory) {
+		$this->documentManagementFactory = $documentManagerFactory;
+		$this->documentManager = $this->documentManagementFactory->create();
+	}
+
+	/**
+	 * @param mixed $document
+	 * @return void
+	 */
+	public function setDocument($document) {
+		if ($document instanceof \Beech\Ehrm\Domain\Model\Document) {
+			$this->documentObject = $document;
+			$this->document = $document->getId();
+		} elseif (is_string($document)) {
+			$this->document = $document;
+			$this->documentObject = $this->documentManager->find('Beech\Ehrm\Domain\Model\Document', $document);
+		}
+	}
+
+	/**
+	 * @return \Beech\Ehrm\Domain\Model\Document
+	 */
+	public function getDocument() {
+		if (!empty($this->document)) {
+			$document = $this->documentManager->find('Beech\Ehrm\Domain\Model\Document', $this->document);
+			if (!empty($document)) {
+				$this->documentObject = $document;
+				$this->document = $document->getId();
+				return $this->documentObject;
+			}
+		}
+
+		$document = new \Beech\Ehrm\Domain\Model\Document();
+		$this->documentManager->persist($document);
+		$this->documentManager->flush();
+		$this->document = $document->getId();
+		$this->documentObject = $document;
+		return $this->documentObject;
+	}
+
+	/**
+	 * @param string $property
+	 * @return mixed
+	 */
+	public function __get($property) {
+		if (property_exists($this, $property)) {
+			return $this->$property;
+		}
+
+		return $this->getDocument()->__get($property);
+	}
+
+	/**
+	 * Magic setter
+	 *
+	 * @param string $property
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function __set($property, $value) {
+		if (substr($property, 0, 5) === 'Flow_') {
+			return;
+		}
+		if (property_exists($this, $property)) {
+			$this->$property = $value;
+		} else {
+			$this->getDocument()->__set($property, $value);
+		}
+	}
+
+	/**
+	 * Magic get* / set* method
+	 *
+	 * @param string $method
+	 * @param array $arguments
+	 * @return mixed
+	 */
+	function __call($method, array $arguments) {
+		if (strlen($method) <= 3) {
+			return NULL;
+		}
+
+		$methodName = '__' . substr($method, 0, 3);
+
+		if ($methodName !== '__set' && $methodName !== '__get') {
+			return NULL;
+		}
+
+		$var = lcfirst(substr($method, 3));
+		return call_user_func_array(array($this, $methodName), array_merge(array($var), $arguments));
+	}
+
+}
+
+?>
--- Packages/Beech/Beech.Party/Classes/Beech/Party/Domain/Model/Person.php
+++ Packages/Beech/Beech.Party/Classes/Beech/Party/Domain/Model/Person.php
@@ -19,29 +19,8 @@ use TYPO3\Flow\Annotations as Flow,
  */
 class Person extends \TYPO3\Party\Domain\Model\Person implements \TYPO3\Flow\Object\DeclaresGettablePropertyNamesInterface {
 
-	/**
-	 * @var string
-	 */
-	protected $document = '';
-
-	/**
-	 * @var \Beech\Ehrm\Domain\Model\Document
-	 * @Flow\Transient
-	 */
-	protected $documentObject;
-
-	/**
-	 * @var \Doctrine\ODM\CouchDB\DocumentManager
-	 * @Flow\Transient
-	 */
-	protected $documentManager;
-
-	/**
-	 * @var \Radmiraal\CouchDB\Persistence\DocumentManagerFactory
-	 * @Flow\Transient
-	 */
-	protected $documentManagementFactory;
-	use \Beech\Ehrm\Domain\ConfigurableModelTrait;
+	use \Beech\Ehrm\Domain\EntityWithDocumentTrait,
+		\Beech\Ehrm\Domain\ConfigurableModelTrait;
 
 	/**
 	 * @var \TYPO3\Party\Domain\Model\PersonName
@@ -49,102 +28,6 @@ class Person extends \TYPO3\Party\Domain\Model\Person implements \TYPO3\Flow\Obj
 	 * @Flow\Validate(type="NotEmpty", validationGroups={"Controller"})
 	 */
 	protected $name;
-
-	/**
-	 * @param \Radmiraal\CouchDB\Persistence\DocumentManagerFactory $documentManagerFactory
-	 * @return void
-	 */
-	public function injectDocumentManagerFactory(\Radmiraal\CouchDB\Persistence\DocumentManagerFactory $documentManagerFactory) {
-		$this->documentManagementFactory = $documentManagerFactory;
-		$this->documentManager = $this->documentManagementFactory->create();
-	}
-
-	/**
-	 * @param mixed $document
-	 * @return void
-	 */
-	public function setDocument($document) {
-		if ($document instanceof \Beech\Ehrm\Domain\Model\Document) {
-			$this->documentObject = $document;
-			$this->document = $document->getId();
-		} elseif (is_string($document)) {
-			$this->document = $document;
-			$this->documentObject = $this->documentManager->find('Beech\Ehrm\Domain\Model\Document', $document);
-		}
-	}
-
-	/**
-	 * @return \Beech\Ehrm\Domain\Model\Document
-	 */
-	public function getDocument() {
-		if (!empty($this->document)) {
-			$document = $this->documentManager->find('Beech\Ehrm\Domain\Model\Document', $this->document);
-			if (!empty($document)) {
-				$this->documentObject = $document;
-				$this->document = $document->getId();
-				return $this->documentObject;
-			}
-		}
-
-		$document = new \Beech\Ehrm\Domain\Model\Document();
-		$this->documentManager->persist($document);
-		$this->documentManager->flush();
-		$this->document = $document->getId();
-		$this->documentObject = $document;
-		return $this->documentObject;
-	}
-
-	/**
-	 * @param string $property
-	 * @return mixed
-	 */
-	public function __get($property) {
-		if (property_exists($this, $property)) {
-			return $this->$property;
-		}
-
-		return $this->getDocument()->__get($property);
-	}
-
-	/**
-	 * Magic setter
-	 *
-	 * @param string $property
-	 * @param mixed $value
-	 * @return void
-	 */
-	public function __set($property, $value) {
-		if (substr($property, 0, 5) === 'Flow_') {
-			return;
-		}
-		if (property_exists($this, $property)) {
-			$this->$property = $value;
-		} else {
-			$this->getDocument()->__set($property, $value);
-		}
-	}
-
-	/**
-	 * Magic get* / set* method
-	 *
-	 * @param string $method
-	 * @param array $arguments
-	 * @return mixed
-	 */
-	function __call($method, array $arguments) {
-		if (strlen($method) <= 3) {
-			return NULL;
-		}
-
-		$methodName = '__' . substr($method, 0, 3);
-
-		if ($methodName !== '__set' && $methodName !== '__get') {
-			return NULL;
-		}
-
-		$var = lcfirst(substr($method, 3));
-		return call_user_func_array(array($this, $methodName), array_merge(array($var), $arguments));
-	}
 
 }
 
