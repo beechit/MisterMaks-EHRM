5b8e7e360fc225af7f52dd5b2e8ae7c6d420f583
karol <karol@beech.it>
2013-02-20 14:00:03 +0100

    [TASK] Create a new form element for country selection
    MM-152
    
    Change-Id: I10d730007cfcd43fca433dc65001c0e98d7cb132

---
+++ Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/FormElements/CountrySelect.php
@@ -0,0 +1,44 @@
+<?php
+namespace Beech\Ehrm\FormElements;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 20/02/13 14:04 AM
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A selection field for country
+ */
+class CountrySelect extends \TYPO3\Form\Core\Model\AbstractFormElement {
+
+	/**
+	 * Location of yaml file with countries
+	 * @var string
+	 */
+	protected $dataFile = 'resource://Beech.Ehrm/Private/Generator/Yaml/country.yaml';
+
+	/**
+	 * Language selected for translations
+	 * @var string
+	 */
+	protected $language = 'nl';
+
+	/**
+	 * @return void
+	 */
+	public function initializeFormElement() {
+		$parsedYaml = \Symfony\Component\Yaml\Yaml::parse($this->dataFile);
+		$countriesArray = array('-' => 'Countries...');
+		foreach ($parsedYaml['country']['values'] as $index => $country) {
+			$countriesArray[$country] = $parsedYaml['country']['translation'][$this->language][$index];
+		}
+		$this->setLabel('Country');
+		$this->setProperty('options', $countriesArray);
+	}
+
+}
+
+?>
---
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/UserInterface/Country.js
@@ -0,0 +1,29 @@
+(function() {
+	'use strict';
+	// size = flag size + spacing
+	var default_size = {
+		w: 20,
+		h: 15
+	};
+
+	function calcPos(letter, size) {
+		return -(letter.toLowerCase().charCodeAt(0) - 97) * size;
+	}
+
+	$.fn.setFlagPosition = function (iso, size) {
+		size || (size = default_size);
+
+		return $(this).css('background-position',
+			[calcPos(iso[1], size.w), 'px ', calcPos(iso[0], size.h), 'px'].join(''));
+	};
+
+	$(function () {
+		// on load:
+		$('.countrySelect').live('change', function() {
+			$(this).parent().find('.country i').setFlagPosition($(this).val());
+		});
+			// set default value to NL
+		$('.countrySelect').val('NL').change();
+	});
+
+}).call(this);
