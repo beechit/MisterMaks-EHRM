fc5b81a891859bd6f4ac7f3ee7c5ceed451d85f8
karol <karol@beech.it>
2013-02-22 15:46:39 +0100

    [TASK] Create a new formelement for nationality
    MM-151
    
    Change-Id: Ia59a5e1fe7ef944db7216db35fbee762d1176bcb

---
+++ Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/FormElements/NationalitySelect.php
@@ -0,0 +1,44 @@
+<?php
+namespace Beech\Ehrm\FormElements;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 22-02-2013 14:16
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A selection field for country
+ */
+class NationalitySelect extends \TYPO3\Form\Core\Model\AbstractFormElement {
+
+	/**
+	 * Location of yaml file with countries
+	 * @var string
+	 */
+	protected $dataFile = 'resource://Beech.Ehrm/Private/Generator/Yaml/nationality.yaml';
+
+	/**
+	 * Language selected for translations
+	 * @var string
+	 */
+	protected $language = 'nl';
+
+	/**
+	 * @return void
+	 */
+	public function initializeFormElement() {
+		$parsedYaml = \Symfony\Component\Yaml\Yaml::parse($this->dataFile);
+		$nationalitiesArray = array('-' => 'Nationality...');
+		foreach ($parsedYaml['nationality'] as $nationality) {
+			$nationalitiesArray[$nationality['identifier']] = $nationality['translation'][$this->language];
+		}
+		$this->setLabel('Nationality');
+		$this->setProperty('options', $nationalitiesArray);
+	}
+
+}
+
+?>
