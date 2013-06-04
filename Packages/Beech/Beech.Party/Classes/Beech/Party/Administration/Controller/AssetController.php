bc3039a88e21743cb69e08bd677f0853c62042cc
karol <karol@beech.it>
2013-06-04 15:54:26 +0200

    [TASK] Rework of Person module
    [+] Add route do frontend of Person module
    [+] Move some templates from administration part to frontend
    [+] Allow reuse of frontend templates inside administration part
    [+] Create partials for Buttons
    [+] Move parts of templates to Views partials
    [+] Create Edit and List template for Company
    
    Change-Id: I7355a419a9808e34f6f705150d71a4e21ff6d927

--- Packages/Beech/Beech.Party/Classes/Beech/Party/Administration/Controller/AssetController.php
+++ Packages/Beech/Beech.Party/Classes/Beech/Party/Administration/Controller/AssetController.php
@@ -15,65 +15,7 @@ use Beech\Party\Domain\Model\Asset as Asset;
  *
  * @Flow\Scope("singleton")
  */
-class AssetController extends \Beech\Ehrm\Controller\AbstractManagementController {
-
-	/**
-	 * @var string
-	 */
-	protected $entityClassName = 'Beech\Party\Domain\Model\Asset';
-
-	/**
-	 * @var string
-	 */
-	protected $repositoryClassName = 'Beech\Party\Domain\Repository\AssetRepository';
-
-	/**
-	 * @var \TYPO3\Flow\I18n\Translator
-	 * @Flow\Inject
-	 */
-	protected $translator;
-
-	/**
-	 * @param \Beech\Party\Domain\Model\Asset $Asset A asset to add
-	 *
-	 * @return void
-	 */
-	public function addAction(\Beech\Party\Domain\Model\Asset $asset) {
-		$asset->setParty($this->persistenceManager->getIdentifierByObject($asset->getParty()));
-		$this->repository->add($asset);
-		$this->view->assign('asset', $asset);
-		$this->view->assign('party', $asset->getParty());
-		$this->view->assign('action', 'add');
-	}
-
-	/**
-	 * @param \Beech\Party\Domain\Model\Asset $asset A asset to update
-	 *
-	 * @return void
-	 */
-	public function updateAction(\Beech\Party\Domain\Model\Asset $asset) {
-		if ($this->getControllerContext()->getRequest()->getArgument('action') === 'remove') {
-			$this->redirect('remove', 'Asset', NULL, array('asset' => $asset, 'person' => $asset->getParty()));
-		} else {
-			$asset->setParty($this->persistenceManager->getIdentifierByObject($asset->getParty()));
-			$this->repository->update($asset);
-			$this->addFlashMessage($this->translator->translateById('Updated.', array(), NULL, NULL, 'Actions', 'Beech.Ehrm'));
-			$this->view->assign('asset', $asset);
-			$this->view->assign('party', $asset->getParty());
-			$this->view->assign('action', 'update');
-		}
-	}
-
-	/**
-	 * @param \Beech\Party\Domain\Model\Asset $asset A asset to remove
-	 * @Flow\IgnoreValidation("$asset")
-	 * @return void
-	 */
-	public function removeAction(\Beech\Party\Domain\Model\Asset $asset) {
-		$asset->setParty(NULL);
-		$this->repository->update($asset);
-		$this->addFlashMessage($this->translator->translateById('Removed.', array(), NULL, NULL, 'Actions', 'Beech.Ehrm'));
-	}
+class AssetController extends \Beech\Party\Controller\AssetController {
 
 }
 
