b6e18d708ea3ab2095b61a879141264851ba7d5a
Frans Saris <frans@beech.it>
2013-06-17 12:28:35 +0200

    [Bugfix] Fixed some (wrong)merge leftovers
    Change-Id: I0f458f53017bced6fc761b3d61596219c5a82df1

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Service/Notification.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Service/Notification.js
@@ -1,46 +1,6 @@
 (function() {
 	'use strict';
 
-	MM.init.afterInitialize.push(function() {
-		App.SocketMessageListeners.pushObject(function(event) {
-			var data = $.parseJSON(event.data);
-			if (data.notifications) {
-				data.notifications.forEach(function(notification) {
-					App.Service.Notification.show(notification.message ? notification.label : notification.message, notification.message ? notification.message : notification.label, notification.type, notification.sticky ? 0 : undefined, notification.closeable ? true : false);
-				});
-			}
-			if (data.signals) {
-				data.signals.forEach(function(signal) {
-					var type, id;
-					if(typeof signal == "string") {
-						var tmp = signal.split(':');
-						type = tmp[0];
-						id = tmp.length > 1 ? tmp[1] : false;
-					} else {
-						type = (signal.type !== "undefined" ? signal.type : "");
-						id = (signal.id !== "undefined" ? signal.id : "");
-					}
-					switch(type) {
-						case 'BeechTaskDomainModelTask':
-							console.log('task ',id)
-							if(id) {
-								var _task = App.BeechTaskDomainModelTask.find(id);
-								if(_task._data) _task.reload();
-							}
-							App.BeechTaskDomainModelPriority.find();
-							break;
-						break;
-
-						default:
-							// unknow signal
-							console.log('Unknow signal from server', signal);
-					}
-
-				});
-			}
-		});
-	});
-
 	App.Service.Notification = Ember.Object.create({
 		INFO: "info",
 		SUCCESS: "success",
