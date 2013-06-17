fd68cd040736e46ab6c6557c39cbc3d9592a3573
karol <karol@beech.it>
2013-06-17 13:34:32 +0200

    [TASK] Implement a computed property like age in the person model
    Change-Id: Ib7f73e6540d8fe0073177796f4af94c110a8b7cf
    Resolves: MM-266

--- Packages/Beech/Beech.Party/Classes/Beech/Party/Domain/Model/Person.php
+++ Packages/Beech/Beech.Party/Classes/Beech/Party/Domain/Model/Person.php
@@ -33,15 +33,37 @@ class Person extends \TYPO3\Party\Domain\Model\Person implements \TYPO3\Flow\Obj
 	 * Get his manager
 	 *
 	 * @todo implement hierarchy until then use the creator
-	 *
 	 * @return \Beech\Party\Domain\Model\Person
 	 */
 	protected function getManager() {
-		if($this->getCreatedBy() !== NULL && $this->getCreatedBy() !== $this) {
+		if ($this->getCreatedBy() !== NULL && $this->getCreatedBy() !== $this) {
 			return $this->getCreatedBy();
 		} else {
 			return NULL;
 		}
+	}
+
+	/**
+	 * Get calculated age of Person
+	 *
+	 * @return integer
+	 */
+	public function getAge() {
+		$dateOfBirth = $this->getDateOfBirth();
+		if (!empty($dateOfBirth)) {
+			list($year, $month, $day) = explode("-", substr($this->getDateOfBirth(), 0, 10));
+			$year_diff = date("Y") - $year;
+			$month_diff = date("m") - $month;
+			$day_diff = date("d") - $day;
+			if ($month_diff < 0) {
+				$year_diff--;
+			}
+			elseif (($month_diff == 0) && ($day_diff < 0)) {
+				$year_diff--;
+			}
+			return $year_diff;
+		}
+		return NULL;
 	}
 }
 
