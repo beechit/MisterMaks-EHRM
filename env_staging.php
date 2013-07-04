7ddb1f829d741e0979b21695376893b965df6b47
Bram Verhaegh <bram@beech.it>
2013-07-04 15:10:08 +0200

    [TASK] Test Deployment 2
    Change-Id: I1aed175fffe4d17964c9635061f0d8572fddead4

--- env_staging.php
+++ env_staging.php
@@ -1,6 +1,6 @@
 <?php
-putenv("DEPLOYMENT_HOST='ehrm.staging.beech.local'");
-putenv("DEPLOYMENT_USERNAME='www-data'");
-putenv("DEPLOYMENT_PATH='/var/www/vhosts/ehrm/'");
-putenv("DEPLOYMENT_BRANCH='1.0'");
+putenv("DEPLOYMENT_HOST=ehrm.staging.beech.local");
+putenv("DEPLOYMENT_USERNAME=www-data");
+putenv("DEPLOYMENT_PATH=/var/www/vhosts/ehrm/");
+putenv("DEPLOYMENT_BRANCH=1.0");
 ?>
