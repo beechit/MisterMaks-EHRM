5644353ecbeaf9a5321d821d0ad2edcfad8af425
Frans Saris <frans@beech.it>
2013-08-12 17:11:56 +0200

    [TASK] Remove GracefulWebSocket
    Websockets are supported by most browsers and
    by Microsoft from IE >= 10.
    
    Because the fallback isn't implemented yet we
    dropped the graceful fallback.
    
    Change-Id: Ib4b0594d6636e9102cc5507374799a51d0f7bea3
    Resolves: MM-386

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Init.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Init.js
@@ -13,45 +13,50 @@ var App;
 		SocketMaxConnectAttempts: 100,
 		SocketFirstConnectAttempt: true,
 		initializeWebSocket: function() {
-			this.SocketConnectAttempts++;
-			this.Socket = $.gracefulWebSocket('ws://'+document.domain+':8000/');
-			this.Socket.onopen = function() {
-				this.send('auth:'+$.cookie('TYPO3_Flow_Session'));
-				console.log('Connection successfully opened (readyState ' + this.readyState + ') attempts: '+App.SocketConnectAttempts);
-				App.SocketConnectAttempts = 0;
-				App.SocketFirstConnectAttempt = false;
-			};
-			this.Socket.onclose = function() {
-				if(this.readyState === 2) {
-					console.log(
-						'Closing... The connection is going through the closing handshake (readyState ' + this.readyState + ')'
-					);
-				} else if(this.readyState === 3) {
-					if(App.SocketFirstConnectAttempt) {
-						// ping websocketserver when we dont get a response at first attempt
-						$.get('/Beech.ehrm/application/pingWebSocketServer');
-						App.SocketFirstConnectAttempt = false;
-					}
-					if(App.SocketConnectAttempts == 0) {
-						App.Service.Notification.showWarning('Connection to the server has been lost trying to reconnect');
-					}
-					if(App.SocketConnectAttempts < App.SocketMaxConnectAttempts) {
-						setTimeout('App.initializeWebSocket()', 1000);
+			if (!window.WebSocket) {
+				App.Service.Notification.showError('Your browser does\'t support websockets. Please upgrade!');
+			} else {
+				this.SocketConnectAttempts++;
+
+				this.Socket = new WebSocket('ws://'+document.domain+':8000/');
+				this.Socket.onopen = function() {
+					this.send('auth:'+$.cookie('TYPO3_Flow_Session'));
+					if (console) console.log('Connection successfully opened (readyState ' + this.readyState + ') attempts: '+App.SocketConnectAttempts);
+					App.SocketConnectAttempts = 0;
+					App.SocketFirstConnectAttempt = false;
+				};
+				this.Socket.onclose = function() {
+					if(this.readyState === 2) {
+						if (console) console.log(
+							'Closing... The connection is going through the closing handshake (readyState ' + this.readyState + ')'
+						);
+					} else if(this.readyState === 3) {
+						if(App.SocketFirstConnectAttempt) {
+							// ping websocketserver when we dont get a response at first attempt
+							$.get('/Beech.ehrm/application/pingWebSocketServer');
+							App.SocketFirstConnectAttempt = false;
+						}
+						if(App.SocketConnectAttempts == 0) {
+							App.Service.Notification.showWarning('Connection to the server has been lost trying to reconnect');
+						}
+						if(App.SocketConnectAttempts < App.SocketMaxConnectAttempts) {
+							setTimeout('App.initializeWebSocket()', 1000);
+						} else {
+							App.Service.Notification.showError('Connection to the server has been lost or could not be opened');
+						}
 					} else {
-						App.Service.Notification.showError('Connection to the server has been lost or could not be opened');
+						if (console) console.log('Connection closed... (unhandled readyState ' + this.readyState + ')');
 					}
-				} else {
-					console.log('Connection closed... (unhandled readyState ' + this.readyState + ')');
-				}
-			};
-			this.Socket.onerror = function(event) {
-				console.log(event.data);
-			};
-			this.Socket.onmessage = function(event) {
-				App.SocketMessageListeners.forEach(function(item) {
-					item.call(this, event);
-				});
-			};
+				};
+				this.Socket.onerror = function(event) {
+					if (console) console.log('Socket error: ',event);
+				};
+				this.Socket.onmessage = function(event) {
+					App.SocketMessageListeners.forEach(function(item) {
+						item.call(this, event);
+					});
+				};
+			}
 		},
 
 		ready: function() {
