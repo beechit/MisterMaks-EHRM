ab28f0cc8ce7ced49cb138dc8ce07993f33b8b95
Bram Verhaegh <bram@beech.it>
2013-08-15 22:25:27 +0200

    [BUGFIX] Commented out imports
    they break data in couch when runned again.
    
    Change-Id: I96142c80f1c2a31a06c773995c835f819adfb8b5

--- initialise-mistermaks.sh
+++ initialise-mistermaks.sh
@@ -3,7 +3,8 @@
 ./flow setup:initialize Beech.it
 ./flow role:import
 ./flow ehrm:user:create admin admin admin admin Beech.Ehrm:Administrator
-./flow import:collection Beech.CLA ContractArticle Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/ContractArticle/ContractArticles.yaml contractArticles.articles --language nl
-./flow import:yaml Beech.CLA JobDescription --sourcePath Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/JobDescription/
-./flow import:yaml Beech.CLA ContractTemplate --sourcePath Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/ContractTemplates/ --pathInYaml contractTemplate
-./flow import:collection Beech.Document DocumentType resource://Beech.Document/Private/Data/DocumentType documentTypes
+# the imports should be done once otherwise they break current data in couchdb.
+#./flow import:collection Beech.CLA ContractArticle Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/ContractArticle/ContractArticles.yaml contractArticles.articles --language nl
+#./flow import:yaml Beech.CLA JobDescription --sourcePath Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/JobDescription/
+#./flow import:yaml Beech.CLA ContractTemplate --sourcePath Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/ContractTemplates/ --pathInYaml contractTemplate
+#./flow import:collection Beech.Document DocumentType resource://Beech.Document/Private/Data/DocumentType documentTypes
