8d64aab678a64c454bc7eac27386260d008e30b1
Bram Verhaegh <bram@beech.it>
2013-08-19 12:18:24 +0200

    [TASK] Stop Reimporting documenttypes on test-enviroment.
    Change-Id: Ic8197df0fe4c4825393eb9fdb7b6dbe4cd47df5f

--- initialise-mistermaks.sh
+++ initialise-mistermaks.sh
@@ -7,4 +7,4 @@
 #./flow import:collection Beech.CLA ContractArticle Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/ContractArticle/ContractArticles.yaml contractArticles.articles --language nl
 #./flow import:yaml Beech.CLA JobDescription --sourcePath Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/JobDescription/
 #./flow import:yaml Beech.CLA ContractTemplate --sourcePath Packages/Beech/Beech.Ehrm.Glastuinbouw/Resources/Private/Data/ContractTemplates/ --pathInYaml contractTemplate
-./flow import:collection Beech.Document DocumentType resource://Beech.Document/Private/Data/DocumentType documentTypes
+#./flow import:collection Beech.Document DocumentType resource://Beech.Document/Private/Data/DocumentType documentTypes
