58dd27cfdcac9a43cf8f0cf6f38c02d75550fed9
karol <karol@beech.it>
2013-08-20 12:11:19 +0200

    [TASK] Create dynamic validation and inputmask on phone numbers
    [*] Support for default, mobile and foreign type
    [*] Move validation rule from yaml to controller
    [*] Modify input mask rules
    
    MM-442
    
    Change-Id: I93dc27b8b736c94ac62ca0c44821224962e17115

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/UserInterface/InputMask.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/UserInterface/InputMask.js
@@ -6,10 +6,19 @@
 			return this.each(function(input_field) {
 				var $this;
 				$this = $(this);
-				if ($this.hasClass('mobilephone')) {
-					$this.inputmask("99 99999999", { "placeholder": "06 ________" });
-				} else if ($this.hasClass('phone')) {
-					$this.inputmask("999 9999999", { "placeholder": "0__ ________" });
+
+				if ($this.hasClass('phone')) {
+					if ($this.hasClass('mobile')) {
+						$this.inputmask("99 99999999", { "placeholder": "06 ________" });
+					} else if ($this.hasClass('foreign')) {
+						$this.inputmask("+99 999999999", { "placeholder": "+__ _________" });
+					} else {
+						$this.inputmask("999 9999999", { "placeholder": "0__ _______" });
+					}
+					$this.parents('.phoneNumber-row').find('.phoneNumberType').unbind('change').on('change', function() {
+						$this.removeClass().addClass('span7 phone').addClass(this.value);
+						$this.applyInputMasks();
+					})
 				} else if ($this.hasClass('postal')) {
 					var inputMaskOptions = {};
 					if (options != undefined && options.country != undefined) {
