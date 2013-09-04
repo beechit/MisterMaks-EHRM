35c70825fc0075da6db3992284d148500bd7b306
karol <karol@beech.it>
2013-09-04 12:53:36 +0200

    [FEATURE] Enable render partials from other packages
    It should be possible to reuse partials from
    other packages by adding packageKey prefix to partial argument
    
    Example:
    <f:render partial="Beech.Document:Form/New" arguments="{_all}"/>
    
    To use this feature, package must use TemplateView from Beech.Ehrm package
    It can be done by adding to Views.yaml, line:
      viewObjectName: 'Beech\Ehrm\View\TemplateView'
    
    MM-522
    
    Change-Id: I58c037d6e8ce63dce4525c6cef0f5a09a1be80fe

--- Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/View/TemplateView.php
+++ Packages/Beech/Beech.Ehrm/Classes/Beech/Ehrm/View/TemplateView.php
@@ -13,6 +13,33 @@ use TYPO3\Flow\Annotations as Flow;
  */
 class TemplateView extends \TYPO3\Fluid\View\TemplateView {
 
+	/**
+	 * Override parent getPartialPathAndFilename method to support
+	 * render partials also from other packages
+	 *
+	 * @param string $partialName The name of the partial
+	 * @return string the full path which should be used. The path definitely exists.
+	 * @throws \TYPO3\Fluid\View\Exception\InvalidTemplateResourceException
+	 */
+	protected function getPartialPathAndFilename($partialName) {
+		$paths = $this->expandGenericPathPattern($this->options['partialPathAndFilenamePattern'], TRUE, TRUE);
+		$partialNameArray = explode(':', $partialName);
+
+		if (count($partialNameArray) === 2) {
+			list($packageKey, $partialName) = $partialNameArray;
+			$paths[] = 'resource://' . $packageKey . '/Private/Partials/@partial.html';
+		}
+
+		foreach ($paths as &$partialPathAndFilename) {
+			$partialPathAndFilename = str_replace('@partial', $partialName, $partialPathAndFilename);
+
+			if (file_exists($partialPathAndFilename)) {
+				return $partialPathAndFilename;
+			}
+		}
+
+		throw new \TYPO3\Fluid\View\Exception\InvalidTemplateResourceException('The template files "' . implode('", "', $paths) . '" could not be loaded.', 1225709595);
+	}
 }
 
 ?>
