dc591bbb346b419abaaf4313a5c26c3c5fcbcfa7
Frans Saris <frans@beech.it>
2013-09-11 11:52:53 +0200

    [BUGFIX] Fix for company routes
    The normal company routes used the administration urls
    Also fixed the link to jobpositions
    
    Change-Id: If61b3e72982cd780588d6c3f552d194b22d53a2d

--- Packages/Beech/Beech.Party/Resources/Public/JavaScript/Controller/Company.js
+++ Packages/Beech/Beech.Party/Resources/Public/JavaScript/Controller/Company.js
@@ -4,19 +4,19 @@
 	// Front
 	App.BeechPartyCompanyController = App.ModuleHandlerAjaxController.extend();
 	App.BeechPartyCompanyIndexController = App.BeechPartyCompanyController.extend({
-		url: MM.url.module.BeechPartyAdministrationCompanyIndex
+		url: MM.url.module.BeechPartyCompanyIndex
 	});
 	App.BeechPartyCompanyNewController = App.BeechPartyCompanyController.extend({
-		url: MM.url.module.BeechPartyAdministrationCompanyNew
+		url: MM.url.module.BeechPartyCompanyNew
 	});
 	App.BeechPartyCompanyShowController = App.BeechPartyCompanyController.extend({
-		url: MM.url.module.BeechPartyAdministrationCompanyShow
+		url: MM.url.module.BeechPartyCompanyShow
 	});
 	App.BeechPartyCompanyEditController = App.BeechPartyCompanyController.extend({
-		url: MM.url.module.BeechPartyAdministrationCompanyEdit
+		url: MM.url.module.BeechPartyCompanyEdit
 	});
 	App.BeechPartyCompanyDeleteController = App.BeechPartyCompanyController.extend({
-		url: MM.url.module.BeechPartyAdministrationCompanyDelete
+		url: MM.url.module.BeechPartyCompanyDelete
 	});
 
 	// Administration
