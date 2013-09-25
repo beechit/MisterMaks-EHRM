2bde56d1cec12acc1ae25761ecfbc04d9ca244cd
Bram Verhaegh <bram@beech.it>
2013-09-25 09:59:55 +0200

    [BUGFIX] Fix bugs in company show
    and some translations.
    
    Change-Id: I7a275b009bcc57fde803868c8f070ace76a431a5

---
+++ Packages/Beech/Beech.Party/Classes/Beech/Party/Administration/Controller/CompanyLogoController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Beech\Party\Administration\Controller;
+
+/*
+ * This source file is proprietary property of Beech Applications B.V.
+ * Date: 23-08-12 14:49
+ * All code (c) Beech Applications B.V. all rights reserved
+ */
+
+use Beech\Document\Domain\Model\DocumentType;
+use TYPO3\Flow\Annotations as Flow;
+use \Beech\Document\Domain\Model\Document;
+
+/**
+ *
+ * @Flow\Scope("singleton")
+ */
+class CompanyLogoController extends \Beech\Party\Controller\CompanyLogoController {
+
+}
+?>
