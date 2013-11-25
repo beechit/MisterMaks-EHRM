5345e38828006b7ebea843dd4c6bb65b97c8a89a
karol <karol@beech.it>
2013-11-25 11:40:55 +0100

    [TASK] Create custom validations for contract wizard
    MM-599
    
    Change-Id: I02d795cf2078ebe351eec5a8131a0251c7a82b37

--- Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/UserInterface/Form.js
+++ Packages/Beech/Beech.Ehrm/Resources/Public/JavaScript/Component/UserInterface/Form.js
@@ -1,6 +1,58 @@
 (function() {
 	'use strict';
 
+	var dates = {
+		convert:function(d) {
+			// Converts the date in d to a date-object. The input can be:
+			//   a date object: returned without modification
+			//  an array      : Interpreted as [year,month,day]. NOTE: month is 0-11.
+			//   a number     : Interpreted as number of milliseconds
+			//                  since 1 Jan 1970 (a timestamp)
+			//   a string     : Any format supported by the javascript engine, like
+			//                  "YYYY/MM/DD", "MM/DD/YYYY", "Jan 31 2009" etc.
+			//  an object     : Interpreted as an object with year, month and date
+			//                  attributes.  **NOTE** month is 0-11.
+			return (
+				d.constructor === Date ? d :
+					d.constructor === Array ? new Date(d[0],d[1],d[2]) :
+						d.constructor === Number ? new Date(d) :
+							d.constructor === String ? new Date(d) :
+								typeof d === "object" ? new Date(d.year,d.month,d.date) :
+									NaN
+				);
+		},
+		compare:function(a,b) {
+			// Compare two dates (could be of any type supported by the convert
+			// function above) and returns:
+			//  -1 : if a < b
+			//   0 : if a = b
+			//   1 : if a > b
+			// NaN : if a or b is an illegal date
+			// NOTE: The code inside isFinite does an assignment (=).
+			return (
+				isFinite(a=this.convert(a).valueOf()) &&
+					isFinite(b=this.convert(b).valueOf()) ?
+					(a>b)-(a<b) :
+					NaN
+				);
+		},
+		inRange:function(d,start,end) {
+			// Checks if date in d is between dates in start and end.
+			// Returns a boolean or NaN:
+			//    true  : if d is between start and end (inclusive)
+			//    false : if d is before start or after end
+			//    NaN   : if one or more of the dates is illegal.
+			// NOTE: The code inside isFinite does an assignment (=).
+			return (
+				isFinite(d=this.convert(d).valueOf()) &&
+					isFinite(start=this.convert(start).valueOf()) &&
+					isFinite(end=this.convert(end).valueOf()) ?
+					start <= d && d <= end :
+					NaN
+				);
+		}
+	}
+
 	/**
 	 * TODO: fix this method
 	 * @param fieldIdentifier
@@ -199,6 +251,15 @@
 			}
 			$('#' + identifier.replace('_','\\.') + '_text').html(value);
 		});
+
+		$(document).on('change', '#contractCreator-article-10000-values_startDate, #contractCreator-article-10000-values_endDate', function() {
+
+			var startDate = $('#contractCreator-article-10000-values_startDate').val().split('-');
+			var endDate = $('#contractCreator-article-10000-values_endDate').val().split('-');
+			if ( dates.compare([startDate[2],startDate[1],startDate[0]], [endDate[2],endDate[1],endDate[0]]) > 0) {
+				alert('Start date cannot be bigger than end date')
+			}
+		});
 	});
 
 	$.fn.extend({
@@ -223,4 +284,8 @@
 			})
 		}
 	});
+	$(document).on('click', '#contractCreator-article-10002-values_hoursAWeek', function() {
+		$(this).inputmask("99.99", { "placeholder": "__.__" });
+	});
+
 }).call(this);
